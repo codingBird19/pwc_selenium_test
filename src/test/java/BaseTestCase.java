2421d1eb233e0391811d8eaffbb610769baa8bfc
jaidkatitir <jaidkatitir@gmail.com>
2021-08-03 21:13:30 +0530

    --parametrized test case --added config.properties --written webdriver methods in BaseTestCase class

--- src/test/java/BaseTestCase.java
+++ src/test/java/BaseTestCase.java
@@ -1,28 +1,110 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTestCase {
-    static WebDriver driver;
+    public static WebDriver driver;
+    public static WebDriverWait webDriverWait;
+    public static Properties config = new Properties();
+    public static FileInputStream fis;
+    public static String browser;
+    public static long timeout;
+
 
     @BeforeTest
-    public void setup(){
-        System.setProperty("webdriver.chrome.driver","E:\\bin\\chromedriver_v92\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
+    public void setup() {
+        if (driver == null) {
+
+            try {
+                fis = new FileInputStream("src//test//resources//config.properties");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                config.load(fis);
+                System.out.println("Config properties loaded!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            browser = config.getProperty("browser");
+            if (browser.equalsIgnoreCase("chrome")) {
+                System.setProperty("webdriver.chrome.driver", config.getProperty("chromedriver_path"));
+                driver = new ChromeDriver();
+                System.out.println("Chrome driver initialized!");
+            } else if (browser.equalsIgnoreCase("firefox")) {
+
+            } else if (browser.equalsIgnoreCase("ie")) {
+
+            }
+
+            timeout = Long.parseLong(config.getProperty("timeouts"));
+            driver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+            webDriverWait = new WebDriverWait(driver, 30);
+            driver.get(config.getProperty("test_url"));
+        }
+
 
     }
 
-    @Test
-    public void initTest(){
+    public boolean isElementPresent(By locator){
+        try{
+            webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            driver.findElement(locator);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
 
     }
+
+    public boolean click(By locator){
+        try{
+            webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            driver.findElement(locator).click();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void enterText(By locator, String value){
+        try{
+            driver.findElement(locator).sendKeys(value);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void dropdownSelectByValue(By locator, String value){
+        WebElement dropdown = driver.findElement(locator);
+        Select select = new Select(dropdown);
+        select.selectByVisibleText(value);
+    }
+    public void selectByValue(By locator, String value){
+        click(locator);
+       driver.findElement(By.xpath("//*[contains(text(),'"+value+"')]")).click();
+    }
+
+
+
 
     @AfterTest
     public void teardown(){
-        driver.quit();
+        if(driver != null)
+            driver.quit();
     }
 }
--- src/test/java/ToolsQA_Test.java
+++ src/test/java/ToolsQA_Test.java
@@ -1,4 +1,6 @@
 import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import pageElements.ToolsqaFormPage_Elements;
 import utils.ExcelUtils;
 
@@ -9,25 +11,33 @@ import java.util.Properties;
 
 public class ToolsQA_Test extends BaseTestCase{
 
-    @Override
+    @Test
     public void initTest()  {
-        driver.get("https://demoqa.com/automation-practice-form/");
         String testdata_path = "src//test//resources//TestData.xlsx";
         Map<String, String> testdata = ExcelUtils.readData(testdata_path, "FormData");
         System.out.println(testdata);
-        driver.findElement(ToolsqaFormPage_Elements.fnameField).sendKeys(testdata.get("FirstName"));
-        driver.findElement(ToolsqaFormPage_Elements.lnameField).sendKeys(testdata.get("LastName"));
-        driver.findElement(ToolsqaFormPage_Elements.email).sendKeys(testdata.get("Email"));
-        driver.findElement(ToolsqaFormPage_Elements.mobile).sendKeys(testdata.get("Mobile"));
-        driver.findElement(ToolsqaFormPage_Elements.gender_f).click();
-        driver.findElement(ToolsqaFormPage_Elements.address).sendKeys(testdata.get("Address"));
+        enterText(ToolsqaFormPage_Elements.fnameField,testdata.get("FirstName"));
+        enterText(ToolsqaFormPage_Elements.lnameField,testdata.get("LastName"));
+        enterText(ToolsqaFormPage_Elements.email,testdata.get("Email"));
 
-        driver.findElement(ToolsqaFormPage_Elements.hobbies_music).click();
+        if(testdata.get("Gender").equalsIgnoreCase("female")){
+            click(ToolsqaFormPage_Elements.gender_f);
+        }else if(testdata.get("Gender").equalsIgnoreCase("male")){
+            click(ToolsqaFormPage_Elements.gender_m);
+        }else click(ToolsqaFormPage_Elements.gender_o);
+        enterText(ToolsqaFormPage_Elements.mobile,testdata.get("Mobile"));
+        click(ToolsqaFormPage_Elements.hobbies_sports);
 
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        //js.executeScript("window.scrollBy(0,350)", "");
 
-        driver.findElement(ToolsqaFormPage_Elements.submit).click();
+
+        enterText(ToolsqaFormPage_Elements.address,testdata.get("Address"));
+        selectByValue(ToolsqaFormPage_Elements.state, testdata.get("State"));
+        selectByValue(ToolsqaFormPage_Elements.city, testdata.get("City"));
+
+
+        click(ToolsqaFormPage_Elements.submit);
+
+        assert(isElementPresent(ToolsqaFormPage_Elements.form_success_msg));
 
 
 
